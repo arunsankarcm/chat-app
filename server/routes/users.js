a21064a57ef9969422ee2208fb60a4042f8da7e1
Arun Sankar <sankarchevoor@gmail.com>
2024-03-31 01:29:00 +0530

    Revert "user signup and login done"
    This reverts commit 0341635bee981e5d4f2679dc249ca843b3681f3d.

--- server/routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const user_controller = require('../controllers/userController');
-
-router.post('/signup', user_controller.signUp);
-router.post('/login', user_controller.userLogin);
-
-module.exports = router;
